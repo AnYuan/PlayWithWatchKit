76541c4b627fb7ed5a06078b76076bec951663a1
AnYuan <donganyuan1010@gmail.com>
2015-03-13 16:11:25 +0800

    init project

---
+++ WatchKit-Basics/HelloWatchKit/HelloWatchKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWatchKit
+//
+//  Created by Anyuan on 15/3/13.
+//  Copyright (c) 2015年 Baidu Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
