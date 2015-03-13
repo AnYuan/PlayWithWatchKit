5ea33b9288649d1e5c238e4d546dc21b5df5d8c6
AnYuan <donganyuan1010@gmail.com>
2015-03-13 16:56:16 +0800

    play with emojis

--- WatchKit-Basics/HelloWatchKit/HelloWatchKit WatchKit Extension/InterfaceController.swift
+++ WatchKit-Basics/HelloWatchKit/HelloWatchKit WatchKit Extension/InterfaceController.swift
@@ -13,6 +13,13 @@ import Foundation
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var label: WKInterfaceLabel!
+    
+    let people = ["😄", "😙", "😔", "😣", "😕", "👯", "💁"]
+    let nature = ["🐣", "🍀", "🌺", "🌴", "⛅️", "🐋", "🐺"]
+    let objects = ["🎁", "⏳", "🍎", "🎵", "💰", "⌚️"]
+    let places = ["✈️", "♨️", "🎭", "🚲", "🎢"]
+    let symbols = ["🔁", "🔀", "⏩", "⏪", "🆒"]
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -23,7 +30,17 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         
-        label.setText("Hello, Apple Watch!")
+//        label.setText("👋, 🍎 ⌚️❗️")
+        
+        // 1
+        let peopleIndex = Int(arc4random_uniform(UInt32(people.count)))
+        let natureIndex = Int(arc4random_uniform(UInt32(nature.count)))
+        let objectsIndex = Int(arc4random_uniform(UInt32(objects.count)))
+        let placesIndex = Int(arc4random_uniform(UInt32(places.count)))
+        let symbolsIndex = Int(arc4random_uniform(UInt32(symbols.count)))
+        
+        // 2
+        label.setText("\(people[peopleIndex])\(nature[natureIndex])\(objects[objectsIndex])\(places[placesIndex])\(symbols[symbolsIndex])")
     }
 
     override func didDeactivate() {
