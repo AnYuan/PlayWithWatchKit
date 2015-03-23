9a7f0f37f56e36cddee39dfb63a33c36b4e1745d
AnYuan <donganyuan1010@gmail.com>
2015-03-23 22:50:00 +0800

    generate three color circles

--- WatchKit-Advanced/ArcGenerator/ArcGenerator/ViewController.swift
+++ WatchKit-Advanced/ArcGenerator/ArcGenerator/ViewController.swift
@@ -15,11 +15,44 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
     
     let generator = ArcGenerator()
+    let width = 10
+    let radius = 60
+    let gap = 2
     
-    let animation = ArcAnimation.progressArcAnimation()
-    animation.name = "progress"
-    animation.initialArc.color = UIColor.magentaColor()
-    animation.initialArc.emptyArcColor = UIColor.magentaColor().colorWithAlphaComponent(0.3)
+    var animation = ArcAnimation.progressArcAnimation()
+    animation.totalFrames = 100
+    animation.name = "progressRed"
+    
+    animation.initialArc = Arc(radius: radius,
+      lineWidth: width, padding: 0,
+      startAngle: 3.0 * M_PI / 2.0, endAngle: 3.0 * M_PI / 2.0,
+      clockwise: true, color: UIColor.magentaColor())
+    animation.initialArc.emptyArcColor =
+      UIColor.magentaColor().colorWithAlphaComponent(0.3)
+    animation.initialArc.lineWidth = width
+    
+    generator.generateArcAnimationFrames(animation)
+    
+    animation = ArcAnimation.progressArcAnimation()
+    animation.totalFrames = 100
+    animation.name = "progressCyan"
+    animation.initialArc = Arc(radius: radius - (width + gap),
+      lineWidth: width, padding: width + gap,
+      startAngle: 3.0 * M_PI / 2.0, endAngle: 3.0 * M_PI / 2.0,
+      clockwise: true, color: UIColor.cyanColor())
+    animation.initialArc.emptyArcColor =
+      UIColor.cyanColor().colorWithAlphaComponent(0.3)
+    generator.generateArcAnimationFrames(animation)
+    
+    animation = ArcAnimation.progressArcAnimation()
+    animation.totalFrames = 100
+    animation.name = "progressGreen"
+    animation.initialArc = Arc(radius: radius - (width + gap) * 2,
+      lineWidth: width, padding: (width + gap) * 2,
+      startAngle: 3.0 * M_PI / 2.0, endAngle: 3.0 * M_PI / 2.0,
+      clockwise: true, color: UIColor.greenColor())
+    animation.initialArc.emptyArcColor =
+      UIColor.greenColor().colorWithAlphaComponent(0.3)
     generator.generateArcAnimationFrames(animation)
   }
   
